b26cad2cb6163ab0828700cb276a9a9ecbd859c6
DG <dgregan@amazon.com>
2021-01-14 17:35:17 +0000

    add post to slack method

--- newsscraper.py
+++ newsscraper.py
@@ -3,16 +3,39 @@
 Reddit News Scraper -  get top X articles from various news subreddits for past day
 '''
 
-import praw, sys, pandas
-from praw import exceptions
-from pathlib import Path
 import configparser
+from pathlib import Path
+
+
+import pandas
+import praw
+from requests import exceptions
+import requests
+import sys
+from praw import exceptions
+
+
+def post_to_slack(msg, url):
+    if len(msg) == 0:
+        sys.exit('Error: No data found')
+    payload = {'news': msg}
+    try:
+        requests.post(url, json=payload)
+    except requests.exceptions.HTTPError as e:
+        raise sys.exit(e)
+    except requests.exceptions.URLRequired as urle:
+        raise sys.exit(urle)
+    except requests.exceptions.Timeout as te:
+        raise sys.exit(te)
 
 
 def get_credentials(service):
     credpath = str(Path.home()) + '/credentials.txt'
     creds = configparser.ConfigParser()
     creds.read(credpath)
+    if service == 'slack':
+        url = creds[service]['webhook_url']
+        return url
     # check if creds are provided / are not blank
     for entry in creds[service]:
         if creds[service][entry].isspace():
@@ -27,8 +50,8 @@ def reddit_client_setup(creds):
         try:
             # try auth
             rclient.user.me()
-        except ValueError:
-            print('ERROR: Invalid credentials provide4d')
+        except praw.exceptions.APIException as err:
+            print(err)
             sys.exit()
     return rclient
 
@@ -51,14 +74,16 @@ def get_news(rclient):
         thread_info['created'].append(thread.created)
         thread_info['body'].append(thread.selftext)
     thread_data = pandas.DataFrame(thread_info)
-    return thread_data
+    return thread_data.to_json()
 
 
 def main():
     creds = get_credentials('reddit')
+    slack_url = get_credentials('slack')
+    # slack_url = 'https://hooks.slack.com/workflows/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'
     rclient = reddit_client_setup(creds)
     news = get_news(rclient)
-    print(news)
+    post_to_slack(news, slack_url)
 
 
 if __name__ == '__main__':
